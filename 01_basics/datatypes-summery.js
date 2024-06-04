38272344b339b59e17d26ac0a341a017fa06acdb
nirmal swami <135805265+nirmalswami@users.noreply.github.com>
2024-06-04 07:25:52 +0000

    datatype summery

---
+++ 01_basics/datatypes-summery.js
@@ -0,0 +1,29 @@
+// primitive
+
+// 7 : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+//console.log(id == anotherId);
+
+
+// Reference (non-primitive)
+// Array, Object, Functions
+
+// array
+const heros = ["spiderman", "saktiman", "batman"]
+
+// object
+let myFuntion = function(){
+    console.log("hello world");
+}
+
+console.log(typeof anotherId);
