107d9b865d1a11afe41657f6a5fa57f07d0f786c
nirmal swami <135805265+nirmalswami@users.noreply.github.com>
2024-06-04 06:25:32 +0000

    changes

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -24,5 +24,8 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 let stringNumber = String(someNumber)
 
-console.log(stringNumber)
-console.log(typeof stringNumber);
+//console.log(stringNumber)
+//console.log(typeof stringNumber);
+
+// ******************* oprations ******************//
+
