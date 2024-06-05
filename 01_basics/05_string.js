dd0e98ea427069f3572efb9f9a7fbc5b62036054
nirmal swami <135805265+nirmalswami@users.noreply.github.com>
2024-06-05 07:07:28 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,22 @@
+const name = "nirmal"
+const repocount = 1
+
+//console.log(`Hello my name is ${name} my repo counut is ${repocount}`);
+
+const gameName = new String ('nirmalds')
+
+// console.log(gameName[7]);
+// console.log(gameName.bold());
+
+const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+const anthorstring = gameName.slice(-6 , 4)
+// console.log(anthorstring);
+
+const newstringOne = "   nirmal     "
+// console.log(newstringOne);
+// console.log(newstringOne.trim());
+
+const url ="https://nirmal.com/nirmal%20swami"
+console.log(url.replace('%20', ''));
