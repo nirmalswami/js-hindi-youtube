af6e7dabf95a7f3787a0308bfd2d4280e83db2ca
nirmal swami <135805265+nirmalswami@users.noreply.github.com>
2024-06-05 07:35:07 +0000

    date and time

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,21 @@
+// dates
+
+const myDate = new Date()
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// const mycreateDate = new Date (2023, 0, 24, 5, 4)
+const mycreateDate = new Date ("01-25-2024")
+// console.log(mycreateDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(mycreateDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate.getDay());
