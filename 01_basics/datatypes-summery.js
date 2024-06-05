c24f1c34818ca6ac81717d7cc5981c5e00e50718
nirmal swami <135805265+nirmalswami@users.noreply.github.com>
2024-06-05 07:07:46 +0000

    summery

--- 01_basics/datatypes-summery.js
+++ 01_basics/datatypes-summery.js
@@ -23,7 +23,28 @@ const heros = ["spiderman", "saktiman", "batman"]
 
 // object
 let myFuntion = function(){
-    console.log("hello world");
+    //console.log("hello world");
 }
 
-console.log(typeof anotherId);
+//console.log(typeof anotherId);
+
+// *******************************************
+
+// stack (primitive), heap(non-primitive)
+
+let officeName ="charumindworks"
+let anothername ="charufashions"
+
+console.log(anothername);
+console.log(officeName);
+
+let userOne = {
+    email : "nirmal@gmail.com",
+    upi : "nirmal@ybl"
+}
+
+let userTwo = userOne
+userTwo.email ="swami@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
